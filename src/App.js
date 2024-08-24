e20e3ecdba9ac8eee72ce6bb2cd2842d631a9e2e
Jegor <140427023+jegorRegularUser@users.noreply.github.com>
2024-08-24 23:25:01 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Store from "./components/steps/Store";
+import Convertor from "./components/hex2rgb/Convertor";
 
 function App() {
+  const [selected, setSelected] = useState('hex2rgb');
+  const btns = ["hex2rgb", "steps",'photo'].map((el, index) => {
+    return (
+      <button
+        className={el === selected ? "btn styled" : "btn"}
+        onClick={()=>{setSelected(el)}}
+        key={index}
+      >
+        {el}
+      </button>
+    );
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tollbar">{btns}</div>
+      {selected==="hex2rgb" && <Convertor />}
+      {selected==="steps" && <Store/> }
+      {selected==="photo" && <Store/> }
     </div>
   );
 }
---
+++ src/components/hex2rgb/Convertor.js
@@ -0,0 +1,34 @@
+import { useRef, useState } from "react";
+import "../../App.css";
+
+
+function Convertor() {
+ 
+  const [value, setValue] = useState('#de9d10');
+  const inputValue = useRef();
+  function hex2RGB(hex) {
+    if (!/^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex)) {
+      return 'Ошибка!'
+    }
+    hex = hex.replace('#', '');
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    const r = parseInt(hex.substr(0, 2), 16);
+    const g = parseInt(hex.substr(2, 2), 16);
+    const b = parseInt(hex.substr(4, 2), 16);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  
+  return (
+    <div className="convertor" style={{background:value}}>
+      <div className="input">
+      <input maxLength={7} ref={inputValue} onChange={()=>{if(inputValue.current.value.length===7){setValue(inputValue.current.value)}}}/>
+      <div>{hex2RGB(value)}</div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Convertor;
